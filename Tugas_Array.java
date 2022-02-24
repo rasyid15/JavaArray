cec76d8dc45aa3229e5e445e02f640cef58ff648
Muhammad Rasyid Ridho <99931023+rasyid15@users.noreply.github.com>
2022-02-24 12:39:25 +0700

    Add files via upload

---
+++ Tugas_Array.java
@@ -0,0 +1,17 @@
+
+package tugas_array;
+
+public class Tugas_Array {
+   public static void main(String[] args) {
+       
+   
+   int no[][]={{2,3,4},{12,13,14},{22,23,24},{32,33,34}}; 
+   
+           for(int i=0;i<4;i++)
+           { for(int j=0;j<3;j++){
+                       System.out.print(no[i][j]+" ");
+           } System.out.println("");
+              }
+   }
+}
+        
---
+++ Tugas_WEBEX_1_0.java
@@ -0,0 +1,11 @@
+
+package tugas_array;
+import java.util.Scanner;
+public class Tugas_WEBEX_1_0 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int []x={1,2,3,4,5,6,7,8,9};
+        System.out.println(x[8]);
+    }
+}
